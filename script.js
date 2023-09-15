6d2cc7b5c1245b172a9e71e7f5a474f7f41c3d68
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-15 21:17:22 +0530

    add event on play button

---
+++ script.js
@@ -0,0 +1,25 @@
+let songIndex = 0;
+let audioElement = new Audio("/songs/1.mp3");
+let masterPlay = document.getElementById("master-play");
+let myProgressBar = document.getElementById("myProgressBar");
+
+let songs = [
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+];
+
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+  }
+});
