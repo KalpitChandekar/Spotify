be8d184f9a75fa3111bd12e663ee447a4727dc05
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-16 18:20:13 +0530

    showing gif while song is play

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let songIndex = 0;
 let audioElement = new Audio("/songs/1.mp3");
 let masterPlay = document.getElementById("master-play");
 let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 
 let songs = [
   { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
@@ -17,9 +18,11 @@ masterPlay.addEventListener("click", () => {
     audioElement.play();
     masterPlay.classList.remove("fa-circle-play");
     masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
   } else {
     audioElement.pause();
     masterPlay.classList.remove("fa-circle-pause");
     masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
   }
 });
