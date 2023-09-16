e8d29658021ebc018eb1ad06ec6565c770c34692
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-16 19:31:34 +0530

    added diff cover & respo song names

--- script.js
+++ script.js
@@ -3,15 +3,60 @@ let audioElement = new Audio("/songs/1.mp3");
 let masterPlay = document.getElementById("master-play");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
-  { songName: "Jadugar", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+  {
+    songName: "Warriyo - Mortals",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Cielo - Huma-Huma",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "DEAF KEV",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: "Different Heaven",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
+  },
+  {
+    songName: "Janji-Heroes-Tonight",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
+  },
+  {
+    songName: "Rabba",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/6.jpg",
+  },
+  {
+    songName: "Sakhiyaan",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/7.jpg",
+  },
+  {
+    songName: "Bhula Dena",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/8.jpg",
+  },
+  {
+    songName: "Tumhari Kasam ",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/9.jpg",
+  },
 ];
+
+songItems.forEach((Element, i) => {
+  Element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  Element.getElementsByClassName("songName")[0].innerHTML = songs[i].songName;
+});
 
 masterPlay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
