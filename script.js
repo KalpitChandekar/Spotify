ecebada1d419a5e763ad21d19c1e3d6e53372daa
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-17 18:52:15 +0530

    feat: add event on previous and next buttons

--- script.js
+++ script.js
@@ -109,3 +109,29 @@ Array.from(document.getElementsByClassName("songItemPlay")).forEach(
     });
   }
 );
+
+document.getElementById("previous").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-circle-play");
+  masterPlay.classList.add("fa-circle-pause");
+});
+
+document.getElementById("next").addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `songs/${songIndex + 1}.mp3`;
+  audioElement.currentTime = 0;
+  audioElement.play();
+  masterPlay.classList.remove("fa-circle-play");
+  masterPlay.classList.add("fa-circle-pause");
+});
